55e6b1f7ecdd7a90b136f4714de54a0657a53077
kilemile26 <harunakilemile@example.com>
2023-12-10 08:31:42 -0800

    task 1

--- models/base_model.py
+++ models/base_model.py
@@ -1,36 +1,63 @@
-"""
-parent class of the Air BnB project
-"""
+#!/usr/bin/python3
+
 from datetime import datetime
 import uuid
 
 class BaseModel:
     """
-    initialization of the project's attributes
-    """
-    def __init__(self, id, created_at, updated_at):
-        self.id = str(uuid.uuid4())
+    Base class for other classes in the Air BnB project.
 
+    Public instance attributes:
+    - id: string - assign with a uuid when an instance is created.
+    - created_at: datetime - assign with the current datetime when an instance is created.
+    - updated_at: datetime - assign with the current datetime when an instance is created and updated every time you change your object.
+
+    Public instance methods:
+    - save(self): Updates the public instance attribute updated_at with the current datetime.
+    - to_dict(self): Returns a dictionary containing all keys/values of __dict__ of the instance.
+    """
+
+    def __init__(self):
+        """
+        Initializes a new instance of the BaseModel class.
+        """
+
+        self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
 
-    """
-    method to set the output format
-    """
-    def __Str__(self):
-        return f"[<class name>] (<self.id>) <self.__dict__>"
 
-    """
-    updates the public instance attribute updated_at with the current datetime
-    """
+    def __Str__(self):
+        """
+        Returns a string representation of the instance.
+
+        Format: [<class name>] (<self.id>) <self.__dict__>
+        """
+
+        return f"[<self.__class__.__name__>] (<self.id>) <self.__dict__>"
+
+
     def save(self):
+        """
+        Updates the public instance attribute updated_at with the current datetime.
+        """
+
         self.updated_at = datetime.now()
 
-    """
-    returns a dictionary containing all keys/values of __dict__ of the instance
-    """
+
     def to_dict(self):
-        return self.__dict__
-        self.__class__
-        created_at = created_at.isoformat()
-        updated_at = updated_at.isoformat()
+        """
+        Returns a dictionary representation of the instance.
+
+        Keys:
+        - __class__: Class name of the object.
+        - created_at: Creation timestamp in ISO format.
+        - updated_at: Last update timestamp in ISO format.
+        """
+
+        return {
+                **self.__dict__,
+                '__class__': self.__class__.__name__,
+                'created_at': self.created_at.isoformat(),
+                'updated_at': self.updated_at.isoformat()
+                }
