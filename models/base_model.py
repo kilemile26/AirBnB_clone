e226b7be4e52fbc2fa1e0dedb39d3153d9d03447
kilemile26 <harunakilemile@example.com>
2023-12-12 06:31:50 -0800

    from dict

--- models/base_model.py
+++ models/base_model.py
@@ -17,14 +17,14 @@ class BaseModel:
     - to_dict(self): Returns a dictionary containing all keys/values of __dict__ of the instance.
     """
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """
         Initializes a new instance of the BaseModel class.
         """
 
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        self.updated_at = self.created_at
 
 
     def __str__(self):
@@ -62,3 +62,27 @@ class BaseModel:
                 'updated_at': self.updated_at.isoformat()
                 **self.__dict__
                 }
+
+
+    @classmethod
+    def from_dict(cls, dict_repr):
+        """
+        Create a new instance of the model from a dictionary representation.
+
+        :param dict_repr: Dictionary representation of the model.
+        :return: New instance of the model.
+        """
+        if '__class__' in dict_repr:
+            del dict_repr['__class__']
+
+        if len(dict_repr) > 0:
+            # Convert created_at and updated_at to datetime objects
+            for key in ('created_at', 'updated_at'):
+                if key in dict_repr:
+                    dict_repr[key] = datetime.strptime(dict_repr[key], '%Y-%m-%dT%H:%M:%S.%f')
+
+            # Create the instance
+            return cls(**dict_repr)
+        else:
+            # Create a new instance with default values
+            return cls()
